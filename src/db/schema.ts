28ee16f4e23ac99b4efcfdd30104309993908cc4
Davis SHYAKA <87414827+davis-shyaka@users.noreply.github.com>
2024-06-13 23:21:25 +0200

    small updates

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -6,11 +6,13 @@ import { pgEnum, pgTable, timestamp, varchar } from "drizzle-orm/pg-core";
  * @description Enum for complexity_level as used on tables such as riddles
  */
 export const complexity_level_enum = pgEnum("complexity_level", [
-	"BEGINNER",
-	"INTERMEDIATE",
-	"ADVANCED",
-	"EXPERT",
-	"MASTER",
+	"LEVEL_1",
+	"LEVEL_2",
+	"LEVEL_3",
+	"LEVEL_4",
+	"LEVEL_5",
+	"LEVEL_6",
+	"LEVEL_7",
 ]);
 
 // ============================================================================
@@ -24,7 +26,7 @@ export const complexity_level_enum = pgEnum("complexity_level", [
 export const riddles = pgTable("riddles", {
 	id: varchar("id", { length: 16 }).primaryKey().notNull(),
 	question: varchar("question", { length: 256 }).notNull(),
-	answer: varchar("answer", { length: 256 }).notNull(),
+	answer: varchar("answer", { length: 16 }).notNull(),
 	categories: varchar("categories", { length: 16 })
 		.array()
 		.notNull()
@@ -49,6 +51,11 @@ export const categories = pgTable("categories", {
 	created_at: timestamp("created_at").notNull().defaultNow(),
 	updated_at: timestamp("updated_at").notNull().defaultNow(),
 });
+
+/**
+ * @name Riddle Shuffle Table
+ * @description
+ */
 
 // ============================================================================
 // Relations
