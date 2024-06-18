9d86036a95613df2daea84f8bf23110a9af75a7e
Davis SHYAKA <87414827+davis-shyaka@users.noreply.github.com>
2024-06-18 21:46:12 +0200

    cleanup

--- src/routes/categories.ts
+++ src/routes/categories.ts
@@ -1,13 +1,19 @@
 import { createRoute, z } from "@hono/zod-openapi";
 import { OpenAPIHono } from "@hono/zod-openapi";
+import { eq } from "drizzle-orm";
 import { drizzle_client } from "~/db";
 import { categories, insert_category_schema } from "~/db/schema";
 import { generate_new_id } from "~/utils/generate-id";
+import {
+  error_responses,
+  success_with_data_schema,
+  success_without_data_schema,
+} from "~/utils/responses";
 
 export const categories_route = new OpenAPIHono<{ Bindings: Bindings }>();
 
 // Route Validation Schemas
-const CategoriesParamsSchema = z.object({
+const categories_params_schema = z.object({
   limit: z
     .string()
     .transform(Number)
@@ -38,29 +44,42 @@ const CategoriesParamsSchema = z.object({
 const get_all_categories = createRoute({
   method: "get",
   path: "/",
-  request: { query: CategoriesParamsSchema },
+  tags: ["Categories"],
+  summary: "Get all categories",
+  description:
+    "Returns a list of all categories. Can return a subset of categories by using the `limit` and `offset` query parameters.",
+  request: { query: categories_params_schema },
   responses: {
     200: {
+      description: "Returns a list of all categories",
       content: {
         "application/json": {
-          schema: insert_category_schema.array().openapi("Categories"),
+          schema: success_with_data_schema(
+            insert_category_schema.array().openapi("Categories"),
+          ),
         },
       },
-      description: "Returns a list of all categories",
     },
+    ...error_responses,
   },
 });
 
 const create_category = createRoute({
   method: "post",
   path: "/",
+  tags: ["Categories"],
+  summary: "Create a new category",
+  description: "Create a new category with a name and description",
   request: {
     body: {
       content: {
         "application/json": {
-          schema: insert_category_schema
-            .omit({ id: true, created_at: true, updated_at: true })
-            .openapi("Categories"),
+          // TODO: Extend this schema to not allow empty strings.
+          schema: insert_category_schema.omit({
+            id: true,
+            created_at: true,
+            updated_at: true,
+          }),
         },
       },
     },
@@ -69,11 +88,12 @@ const create_category = createRoute({
     201: {
       content: {
         "application/json": {
-          schema: z.object({}),
+          schema: success_without_data_schema,
         },
       },
-      description: "Created product.",
+      description: "Category created.",
     },
+    ...error_responses,
   },
 });
 
@@ -82,17 +102,48 @@ categories_route.openapi(get_all_categories, async (c) => {
   const { limit, offset } = c.req.valid("query");
   const db = drizzle_client(c.env.DATABASE_URL);
   const data = await db.query.categories.findMany({ limit, offset });
-  return c.json(data, 200);
+  return c.json(
+    { success: true, data, message: `Returned ${data.length} categories` },
+    200,
+  );
 });
 
 categories_route.openapi(create_category, async (c) => {
   const { name, description } = c.req.valid("json");
   const id = generate_new_id("category");
   const db = drizzle_client(c.env.DATABASE_URL);
+  const existing_category = await db.query.categories.findFirst({
+    where: eq(categories.name, name),
+    columns: { name: true },
+  });
+  if (existing_category) {
+    return c.json(
+      {
+        success: false,
+        error: { status: 400, message: `Category '${name}' already exists` },
+      },
+      400,
+    );
+  }
   const data = await db.insert(categories).values({
     id,
     name,
     description,
   });
-  return c.json(data, 201);
+  if (data.rowCount === 0) {
+    return c.json(
+      {
+        error: { status: 500, message: "Category not created" },
+        success: false,
+      },
+      500,
+    );
+  }
+  return c.json(
+    {
+      success: true,
+      message: "Category created successfully",
+    },
+    201,
+  );
 });
