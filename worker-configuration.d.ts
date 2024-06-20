79ce7309057ab8ec07fa9cf8f736ce97e5183520
Davis SHYAKA <87414827+davis-shyaka@users.noreply.github.com>
2024-06-20 16:35:26 +0200

    simplify

--- worker-configuration.d.ts
+++ worker-configuration.d.ts
@@ -1,8 +1,10 @@
+import type { Session, User } from "lucia";
+
 type Bindings = {
 	DATABASE_URL: string;
 };
 
 type Variables = {
-	user: import("lucia").User | null;
-	session: import("lucia").Session | null;
+	user: User | null;
+	session: Session | null;
 };
